f3fbe93e3749ada37c99e2c1edf708e86b75c33a
Daisuke Itabashi <itabashi.d@gmail.com>
2021-10-16 22:01:04 +0900

    Initial commit

---
+++ Shared/KotlinNativeTrialXcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  KotlinNativeTrialXcodeApp.swift
+//  Shared
+//
+//  Created by Daisuke Itabashi on 2021/10/16.
+//
+
+import SwiftUI
+
+@main
+struct KotlinNativeTrialXcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
